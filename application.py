641615ea8d179022dc8a0caf85649549aa3f707a
Vincent van Triest <vincentvantriest@gmail.com>
2018-01-22 11:50:05 +0000

    helpers en application update

--- application.py
+++ application.py
@@ -76,7 +76,7 @@ def login():
         session["user_id"] = rows[0]["id"]
 
         # redirect user to home page
-        return redirect(url_for("index"))
+        return redirect(url_for("game"))
 
     # else if user reached route via GET (as by clicking a link or via redirect)
     else:
