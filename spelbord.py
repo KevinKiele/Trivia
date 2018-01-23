dcdcf98eee7076e61062abd12a0ac33772633ecc
Vincent van Triest <vincentvantriest@gmail.com>
2018-01-23 13:02:30 +0000

    spelbord begonnen

---
+++ spelbord.py
@@ -0,0 +1,23 @@
+import random
+
+## SPELBORD
+breedte = 80
+treasurex = 1
+
+class Kamer:
+    def __init__(self):
+        self.Treasure = False
+
+spelbord = [[Kamer() for n in range (10)] for n in range (10)]
+
+for n in range (treasurex):
+
+def setup():
+    size(800,800)
+
+
+def draw():
+
+
+
+
