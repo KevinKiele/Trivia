e2815df08284daeb60b8bad24d9e6a83ab4507b2
Vincent van Triest <vincentvantriest@gmail.com>
2018-01-24 14:51:51 +0000

    spelbord css + java begin en aantal kleine fixes

--- templates/spelbordjava.js
+++ templates/spelbordjava.js
@@ -1,14 +1,12 @@
 var canvas = document.querySelector('canvas');
 
-canvas.width = 150;
-canvas.height = 150;
+canvas.width = 400;
+canvas.height = 400;
 
 var c = canvas.getContext('2d');
 
 c.fillRect(50, 50, 50, 50);
-c.fillRect(105, 50, 50, 50);
-c.fillRect(55, 100, 50, 50);
-c.fillRect(110, 110, 50, 50);
+
 
 
 
