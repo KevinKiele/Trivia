f243d6c64eab94eb43a3da076bc3a0666d582358
Vincent van Triest <vincentvantriest@gmail.com>
2018-01-30 14:49:28 +0000

    Rerouting, verschillende quizes en navbar overal zichtbaar

--- static/app_Algemeen.js
+++ static/app_Algemeen.js
@@ -37,7 +37,7 @@ function showProgress() {
 function showScores() {
     var gameOverHTML = "<h1>Result</h1>";
     gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
-    gameOverHTML += "<h3>Return to Lobby Selection</h>";
+    gameOverHTML += "<h3>Thanks for playing and well done! If you'd like to fire up a new game, click Game Selection in the Navigation bar.</h>";
     var element = document.getElementById("quiz");
     element.innerHTML = gameOverHTML;
 };
